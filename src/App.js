a380f26efeeefaac7888d5843ce2ff1492fc55b9
Ameer Hamza <ameerhamza205242@gmail.com>
2020-12-28 13:45:24 +0500

    adding a css file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import './index.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello Friends</h1>
       <h2>My Name is Ameer Hamza.</h2>
       <h2>It is just a checking page for auto deploy and host a react base app.</h2>
     </div>
